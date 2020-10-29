9bc24f5f932576e7da40221bf28e8b63393f5787
Guilherme Frota <guiquintelas96@gmail.com>
2020-10-29 19:42:05 -0300

    add playgorund url when server is up

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    console.log('playground: http://localhost:3000/graphql');
+  });
 }
 bootstrap();

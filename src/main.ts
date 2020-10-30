fba1b900dcdc0c1ab3a9bbadd1f2cafd1f571cc9
Guilherme Frota <guiquintelas96@gmail.com>
2020-10-30 10:38:20 -0300

    fix eslint warning

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   await app.listen(3000, () => {
+    // eslint-disable-next-line no-console
     console.log('playground: http://localhost:3000/graphql');
   });
 }
